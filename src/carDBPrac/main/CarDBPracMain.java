cc623f3b635a21a65115c7df63dfe474c251a7f4
gonnichiwa <keepthefaith00@gmail.com>
2018-01-31 17:13:24 +0900

    main에서 carInfo select 실행

--- src/carDBPrac/main/CarDBPracMain.java
+++ src/carDBPrac/main/CarDBPracMain.java
@@ -1,9 +1,10 @@
 package carDBPrac.main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CarDBPracMain {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// 메뉴의 번호를 입력받아 지정한 번호 이외 다른 번호를 입력하면
 		// "잘못 입력하셨습니다" 출력 후 다시 입력 받게 함
 		Scanner sc = new Scanner(System.in);
@@ -18,7 +19,13 @@ public class CarDBPracMain {
 			System.out.println("======================");
 			int inputNum = sc.nextInt();
 			if(inputNum == 1) { // 자동차 정보 조회
+				ArrayList<CarInfo> list = dao.getCarInfoList();
 				
+				// 조회한 데이터 뿌리기
+				for (int i = 0; i < list.size(); i++) {
+					CarInfo ci = list.get(i);
+					System.out.println(ci.toString());
+				}
 				break;
 			} else if(inputNum == 2) { // 자동차 정보 입력
 
