3173cf86207a61cf763ad69e84556769fd16c928
gonnichiwa <keepthefaith00@gmail.com>
2018-02-01 11:06:00 +0900

    where절 쿼리 추가

--- sqlFile/CarInfoDDL.sql
+++ sqlFile/CarInfoDDL.sql
@@ -1,5 +1,5 @@
 /*
- * CarInfoDDL.sql
+ * CarInfoDDL.sql : DDL,DML 다 포함된 sql파일. 이름 잘 못정했당.. ㅜㅜ
  * */
 
 create table carInfo (
@@ -18,6 +18,14 @@ insert into carInfo(ciNum,ciName,ciMaker,ciPrice) values (seq_carInfo_ciNum.next
 insert into carInfo(ciNum,ciName,ciMaker,ciPrice) values (seq_carInfo_ciNum.nextval,'k3','기아',2200);
 insert into carInfo(ciNum,ciName,ciMaker,ciPrice) values (seq_carInfo_ciNum.nextval,'말리부','GM',2700);
 insert into carInfo(ciNum,ciName,ciMaker,ciPrice) values (seq_carInfo_ciNum.nextval,'그랜저','현대',5000);
+
+-- carInfo 테이블의 전체 자료 조회
+select * from carInfo;
+
+-- ciNum이 5인 carInfo 칼럼 모두를 조회 하시오 (where절)
+select * from carInfo where ciNum = 5;
+
+
 
 
 
