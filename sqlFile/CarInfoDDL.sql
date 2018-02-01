eda6108d6b1bc9e8e33084f618e6dd1a417fdb4c
gonnichiwa <keepthefaith00@gmail.com>
2018-02-01 13:25:26 +0900

    carInfo테이블 상세보기 기능을 위해 수정

--- sqlFile/CarInfoDDL.sql
+++ sqlFile/CarInfoDDL.sql
@@ -3,10 +3,13 @@
  * */
 
 create table carInfo (
-	ciNum int primary key,
-	ciName varchar2(30) not null,
-	ciMaker varchar2(30) not null,
-	ciPrice int not null
+	ciNum int primary key, -- 정보번호
+	ciName varchar2(30) not null, -- 차명
+	ciMaker varchar2(30) not null, -- 제조사
+	ciPrice int not null, -- 가격
+  ciColor varchar2(20) not null, -- 차색깔
+  ciWidth int not null, -- 폭(cm)
+  ciHeight int not null -- 높이(cm)
 );
 
 create sequence seq_carInfo_ciNum
@@ -14,17 +17,22 @@ start with 1
 increment by 1
 nomaxvalue;
 
-insert into carInfo(ciNum,ciName,ciMaker,ciPrice) values (seq_carInfo_ciNum.nextval,'아반떼','횬대',2000);
-insert into carInfo(ciNum,ciName,ciMaker,ciPrice) values (seq_carInfo_ciNum.nextval,'k3','기아',2200);
-insert into carInfo(ciNum,ciName,ciMaker,ciPrice) values (seq_carInfo_ciNum.nextval,'말리부','GM',2700);
-insert into carInfo(ciNum,ciName,ciMaker,ciPrice) values (seq_carInfo_ciNum.nextval,'그랜저','현대',5000);
+insert into carInfo(ciNum,ciName,ciMaker,ciPrice,ciColor,ciWidth,ciHeight) 
+values (seq_carInfo_ciNum.nextval,'아반떼','횬대',2000,'흰색',500,150);
+insert into carInfo(ciNum,ciName,ciMaker,ciPrice,ciColor,ciWidth,ciHeight) 
+values (seq_carInfo_ciNum.nextval,'k3','기아',2200,'검은색',480,160);
+insert into carInfo(ciNum,ciName,ciMaker,ciPrice,ciColor,ciWidth,ciHeight) 
+values (seq_carInfo_ciNum.nextval,'말리부','GM',2700,'흰색',520,165);
+insert into carInfo(ciNum,ciName,ciMaker,ciPrice,ciColor,ciWidth,ciHeight) 
+values (seq_carInfo_ciNum.nextval,'그랜저','현대',5000,'검은색',550,200);
 
 -- carInfo 테이블의 전체 자료 조회
 select * from carInfo;
 
+drop table carInfo;
+
 -- ciNum이 5인 carInfo 칼럼 모두를 조회 하시오 (where절)
 select * from carInfo where ciNum = 5;
-
 
 
 
